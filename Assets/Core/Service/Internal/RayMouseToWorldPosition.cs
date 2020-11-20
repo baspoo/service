a2017f641e354330fa5c984155a3e1191fc499d3
ONEMOBY\ritichai <ritichai@1moby.com>
2020-11-20 17:46:41 +0700

    add service -spin3D -raymouse

---
+++ Assets/Core/Service/Internal/RayMouseToWorldPosition.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayMouseToWorldPosition : MonoBehaviour
+{
+    public LayerMask layerMask;
+    Ray ray;
+    RaycastHit hitData;
+    Vector3 worldPosition;
+    public delegate void onclick(Vector3 worldposition);
+    public onclick OnClick;
+    public onclick OnUpdate;
+    void Update()
+    {
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hitData, 1000, layerMask))
+        {
+            worldPosition = hitData.point;
+            if (OnUpdate != null)
+                OnUpdate(worldPosition);
+            if (Input.GetMouseButtonDown(0)) { 
+                if (OnClick != null)
+                    OnClick(worldPosition);
+            }
+        }
+    }
+
+}
---
+++ Assets/Core/Service/Internal/Spin3D.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spin3D : MonoBehaviour
+{
+    public Transform target;
+    public float mouseSpeedMultiplier = 8;
+    public float smoothSpeed = 0.04f;
+    private float mouseX;
+    void OnMouseDrag()
+    {
+        mouseX += Input.GetAxis("Mouse X") * mouseSpeedMultiplier;
+    }
+
+    void LateUpdate() //Cause we are using Lerp function
+    {
+        if (target == null)
+            target = transform;
+        target.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0, -mouseX, 0), smoothSpeed);
+    }
+}
